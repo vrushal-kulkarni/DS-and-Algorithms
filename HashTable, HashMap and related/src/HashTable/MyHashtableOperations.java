4ccd868b6a722474fc342e94b3acd97b9fc5e6eb
Vrushal <kulkarni.v@husky.neu.edu>
2016-07-23 04:17:32 -0400

    HashTable done

---
+++ HashTable, HashMap and related/src/HashTable/MyHashtableOperations.java
@@ -0,0 +1,34 @@
+package HashTable;
+
+/*  Below example shows basic operations on Hashtable like creating hashtable object, 
+ * adding key-value pair, getting the value based on key, checking hashtable is empty or not, 
+ * removing an element, and size of the hashtable.*/
+
+import java.util.Hashtable;
+import java.util.Set;
+
+public class MyHashtableOperations {
+ 
+    public static void main(String a[]){
+        //Create hashtable instance
+        Hashtable<String,String> ht = new Hashtable<String,String>();
+        //add key-value pair to hashtable
+        ht.put("first", "FIRST INSERTED");
+        ht.put("second", "SECOND INSERTED");
+        ht.put("third","THIRD INSERTED");
+        System.out.println(ht);
+        //getting value for the given key from hashtable
+        System.out.println("Value of key 'second': "+ht.get("second"));
+        System.out.println("Is Hashtable empty? "+ht.isEmpty());
+        ht.remove("third");
+        System.out.println(ht);
+        System.out.println("Size of the Hashtable: "+ht.size());
+        
+        /*for iterating through hastable*/
+        Set<String> keys = ht.keySet();
+        for(String key: keys){
+            System.out.println("Value of "+key+" is: "+ht.get(key));
+        }
+        
+    }
+}
